504012f7f59fa53bc2836e71d5fa635abe7d0fda
mymy8298 <55151859+mymy8298@users.noreply.github.com>
2020-01-02 20:23:13 +0700

    Update dangky.php

--- dangky.php
+++ dangky.php
@@ -1,147 +1,73 @@
-<?php 
-    
-    if (isset($_POST['dangky'])){
-        //Nhúng file kết nối với database
-        include('config.php');
-            
-        //Khai báo utf-8 để hiển thị được tiếng việt
-        // header('Content-Type: text/html; charset=UTF-8');
-            
-        //Lấy dữ liệu từ file dangky.php
-        $TenTK   = addslashes($_POST['txtTenTK']);
-        $matkhau   = addslashes($_POST['txtmatkhau']);
-        $email      = addslashes($_POST['txtEmail']);
-        $tenuser   = addslashes($_POST['txttenuser']);
-        $ngaysinh   = addslashes($_POST['txtngaysinh']);
-        $gioitinh        = addslashes($_POST['txtgioitinh']);
-        $sodt        = addslashes($_POST['txtsodt']);
-            
-        //Kiểm tra người dùng đã nhập liệu đầy đủ chưa
-        if (!$TenTK || !$matkhau || !$email || !$tenuser || !$ngaysinh || !$gioitinh || !$sodt)
-        {
-            echo "Vui lòng nhập đầy đủ thông tin. <a href='javascript: history.go(-1)'>Trở lại</a>";
-            exit;
-        }
-            
-            // Mã khóa mật khẩu
-            // $matkhau = md5($matkhau);
-            
-        //Kiểm tra tên đăng nhập này đã có người dùng chưa
-        if (mysqli_num_rows(mysqli_query($db,"SELECT TenTK FROM user WHERE TenTK='$TenTK'")) > 0){
-            echo "Tên đăng nhập này đã có người dùng. Vui lòng chọn tên đăng nhập khác. <a href='javascript: history.go(-1)'>Trở lại</a>";
-            exit;
-        }
-            
-        //Kiểm tra email có đúng định dạng hay không
-        // if (!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$", $email))
-        // {
-        //     echo "Email này không hợp lệ. Vui lòng nhập email khác. <a href='javascript: history.go(-1)'>Trở lại</a>";
-        //     exit;
-        // }
-            
-        //Kiểm tra email đã có người dùng chưa
-        if (mysqli_num_rows(mysqli_query($db,"SELECT email FROM user WHERE email='$email'")) > 0)
-        {
-            echo "Email này đã có người dùng. Vui lòng chọn Email khác. <a href='javascript: history.go(-1)'>Trở lại</a>";
-            exit;
-        }
-        //Kiểm tra dạng nhập vào của ngày sinh
-        // if (!ereg("^[0-9]+/[0-9]+/[0-9]{2,4}", $ngaysinh))
-        // {
-        //         echo "Ngày tháng năm sinh không hợp lệ. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
-        //         exit;
-        // }
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Trang đăng ký</title>
+  <link rel="stylesheet" href="css/login/style.css">
 
-        // if (!eregi("^[0-9])", $sodt))
-        // {
-        //     echo "Số điện thoại này không hợp lệ. Vui lòng nhập lại. <a href='javascript: history.go(-1)'>Trở lại</a>";
-        //     exit;
-        // }
-            
-        //Lưu thông tin thành viên vào bảng
-        @$adduser = mysqli_query($db,"
-            INSERT INTO user (
-                TenTK,
-                matkhau,
-                email,
-                tenuser,
-                ngaysinh,
-                gioitinh,
-                sodt
-            )
-            VALUE (
-                '{$TenTK}',
-                '{$matkhau}',
-                '{$email}',
-                '{$tenuser}',
-                '{$ngaysinh}',
-                '{$gioitinh}',
-                '{$sodt}'
-            )
-        ");
-                            
-        //Thông báo quá trình lưu
-        if ($adduser)
-        {
-            header("Location: TrangChu.php");
-            exit;
-        } else {
-            echo "Có lỗi xảy ra trong quá trình đăng ký. <a href='dangky.php'>Thử lại</a>";
-        }
-    }     
-?>
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<!--
+Author: Colorlib
+Author URL: https://colorlib.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" href="css/style.css">
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Trang đăng ký</title>
-    </head>
-
-    <body>
-        <!-- <h1>Trang đăng ký thành viên</h1> -->
-        <div class="container">
-            <section id="content">
-            <form action="dangky.php" method="POST">
-                <!-- <table cellpadding="0" cellspacing="0" border="1"> -->
-                    <div>
-                        <input type="text" name="txtTenTK" splaceholder="Tên đăng nhập" />
-                    </div>
-                    <div>
-                        <input type="password" name="txtmatkhau" placeholder="Mật Khẩu" />
-                    </div>
-                    <div>
-                        <input type="text" name="txtEmail"  placeholder="Email" />
-                    </div>
-                    <div>
-                        <input type="text" name="txttenuser"  placeholder="Họ và tên" />
-                    </div>
-                    <div>
-                        <input type="text" name="txtngaysinh"  placeholder="Ngày sinh" />
-                    </div>
-                    <div>
-                        <input type="text" name="txtgioitinh"  placeholder="Giới tính" />
-                    </div>
-                    <div>
-                    <input type="text" name="txtsodt"  placeholder=" Số Điện Thoại" />
-                    </div>
-                    <div>
-                        <input type="submit" name="dangky" value="Đăng ký" />
-                        
-                        <input type="reset" value="Nhập lại" />
-                    </div>
-        </form>
-        </section>
-
-        <script src="js/index.js"></script>
-        <?php
-
-            // Nếu không phải là sự kiện đăng ký thì không xử lý
-            if (!isset($_POST['dangky'])){
-                die('');
-            } 
-        ?>
-        
-    </body>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Custom Theme files -->
+<link href="css/login/style.css" rel="stylesheet" type="text/css" media="all" />
+<!-- //Custom Theme files -->
+<!-- web font -->
+<link href="//fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,700,700i" rel="stylesheet">
+<!-- //web font -->
+</head>
+<body>
+	<!-- main -->
+	<div class="main-w3layouts wrapper">
+		<h1>Đăng ký tài khoản</h1>
+		<div class="main-agileinfo">
+			<div class="agileits-top">
+                <section id="content">
+                    <form action="dangky.php" method="POST">
+                        <!-- <table cellpadding="0" cellspacing="0" border="1"> -->
+                            <input class="text" type="text" name="txtTenTK" placeholder="Tên đăng nhập" required="" />                         
+                            <input class="text email" type="password" name="txtmatkhau" placeholder="Mật Khẩu" required=""/>
+                            <input class="text" type="text" name="txtEmail"  placeholder="Email" required=""/>
+                            <input class="text email" type="text" name="txttenuser"  placeholder="Họ và tên" required=""/>
+                            <input class="text" type="text" name="txtgioitinh"  placeholder="Giới tính" required=""/>
+                            <input class="text email" type="text" name="txtsodt"  placeholder=" Số Điện Thoại" required=""/>
+                            
+                            <input type="submit" name="dangky" value="Đăng ký" />
+                    </form>
+                </section>
+			</div>
+		</div>
+		<ul class="colorlib-bubbles">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+	</div>
+	<!-- //main -->
+<script src="js/index.js"></script>
+    <?php
+       include('xldangky.php');
+    ?>
+</body>
+</html>
+<!-- partial -->
+</body>
 </html>
